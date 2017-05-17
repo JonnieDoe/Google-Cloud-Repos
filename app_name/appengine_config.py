adda6c114e373b8a8851a16c9bee11d48784ee61
DC <notify.mynews@gmail.com>
2017-05-17 12:17:37 +0300

    Update_Template_0.0.1

--- app_name/appengine_config.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os.path
-
-
-"""`appengine_config` gets loaded when starting a new application instance."""
-
-# add `lib` subdirectory to `sys.path`, so our `main` module can load
-# third-party libraries.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
--- app_name/tmp/test.py
+++
@@ -1,5 +0,0 @@
-users = ['test']
-
-if __name__ == '__main__':
-    for admin in users:
-        print(str(admin))
